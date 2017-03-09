cb165cb033a40c53fe925f1a8163a9e65f213bae
David Hahn <david@40digits.com>
2017-03-08 22:42:51 -0600

    Add discourse event listener for topic_created

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,19 @@
 # about: Integration with apt-crowd api
 # version: 0.0.1
 # authors: David Hahn
+
+DiscourseEvent.on(:topic_created) do |topic, _, user|
+  post = topic.first_post;
+
+  requestBody = {
+    title: topic.title,
+    message_body: post.raw,
+    category_id: topic.category_id,
+    author_id: topic.user_id,
+    post_id: post.id,
+    topic_id: topic.id,
+    tags: ['tag1', 'tag2']
+  } 
+
+  raise requestBody.inspect
+end
